2f9810bd34e70b2bd12362d47ed8bf23c8a924d3
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-04 08:55:54 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-var input = $("#value-input")
+var input = $("3.14159265358979323846")
 
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
     var valueToSave = input.val();
     console.log("hi");
-    storeValue("little_apple", valueToSave);
-    console.log(valueToSave)
   }
 })
 
-onNewValue("little_apple", function(value) {
+onNewValue("3.141592653589793238462643383", function(value) {
   $("#value-output").html(value)
 })
